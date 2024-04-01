5fd3e340b024d0e98f3e174d3bd3b4ec93f92947
Wesley Smith <yelsew414@gmail.com>
2024-04-01 17:57:51 -0600

    initial commit

---
+++ react_app/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store'; // Adjust the path as necessary
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import TextInputWithSubmit from './components/TextInputWithSubmit'; // Adjust the path as necessary
+import About from './pages/About'; // Assume we have an About component
+import AddFood from './pages/AddFood';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<AddFood />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ react_app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This app will help you track your food sensitivies over time!</p>
+    </div>
+  );
+}
+
+export default About;
